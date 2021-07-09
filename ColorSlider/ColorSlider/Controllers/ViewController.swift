8a4142af3015efc6c2b8264fcf00771e9f70585e
gbasovv <gbasovv@gmail.com>
2021-07-09 12:43:30 +0300

    Commit 1

--- ColorSlider/ColorSlider/Controllers/ViewController.swift
+++ ColorSlider/ColorSlider/Controllers/ViewController.swift
@@ -7,21 +7,33 @@
 
 import UIKit
 
+protocol FirstViewControllerDelegate: AnyObject {
+    func update(background: UIColor)
+}
+
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var changeBtn: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
     }
-    
+
     private func setUI() {
         changeBtn.layer.cornerRadius = 15
     }
 
-    @IBAction func changeBtnTapped() {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let destination = segue.destination as? SecondViewController else { return }
+        destination.delegate = self
     }
-    
+
+}
+
+extension ViewController: FirstViewControllerDelegate {
+    func update(background: UIColor) {
+        self.view.backgroundColor = background
+    }
 }
 
